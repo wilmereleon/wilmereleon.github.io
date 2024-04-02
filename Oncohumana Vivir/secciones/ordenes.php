539076ad308de18cfc5a3130d0a5cb1c902e4bf5
Wílmer E. León Díaz <42726764+wilmereleon@users.noreply.github.com>
2024-04-02 11:48:53 -0500

    Delete Oncohumana Vivir directory
    Borrado

--- Oncohumana Vivir/secciones/ordenes.php
+++
@@ -1,9 +0,0 @@
-<?php include('../plantillas/header.php'); ?>
- 
-
-
-Listado de ordenes para generar impresión (incluye remisiones, historiales médicos, laboratorios e incapacidades)
-
-
-    
-<?php include('../plantillas/footer.php'); ?>
--- Oncohumana Vivir/secciones/vista_pacientes.php
+++
@@ -1,9 +0,0 @@
-<?php include('../plantillas/header.php'); ?>
- 
-
-
-Interfaz del usuario paciente al ingresar
-
-
-    
-<?php include('../plantillas/footer.php'); ?>
