6315ee13cbae340885d2a15ade3d171cf4d2ff65
Wílmer E. León Díaz <42726764+wilmereleon@users.noreply.github.com>
2024-05-30 13:11:20 -0500

    Delete ProyectoBoli directory

--- ProyectoBoli/install.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Este es el código PHP que guarda los datos en una hoja de Excel y genera un PDF
-// Se requiere instalar las librerías PHPExcel y TCPDF
-require_once 'PHPExcel-1.8.2/Classes/PHPExcel.php';
-require_once 'TCPDF-6.4.1/tcpdf.php';
-
-echo 'Proceso efectuado';
-
-?>
